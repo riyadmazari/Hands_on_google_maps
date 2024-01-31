7e621e099dd1a17c9c564b86fa93b77e0c8031c8
riyadmazari <rmazari.ieu2021@student.ie.edu>
2024-01-31 22:33:19 +0100

    everything works this way

--- utils/preprocessing.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-def preprocess_frame(frame, img_size=(64, 64)):
-    frame = cv2.resize(frame, img_size)
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    frame = frame / 255.0
-    frame = frame.reshape(1, img_size[0], img_size[1], 1)
-    return frame
